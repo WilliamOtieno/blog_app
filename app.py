d20f08004d9d64284db50036bb52b1dbc0e295c2
William Otieno <jimmywilliamotieno@gmail.com>
2020-05-26 16:26:28 +0300

    Added html template

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from datetime import datetime
+
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -34,6 +36,25 @@ def post():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+
+@app.route('/addpost', methods=['POST'])
+def addpost():
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
+
+    post = Blogpost(title=title, subtitle=subtitle, author=author, content=content, date_posted=datetime.now())
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
