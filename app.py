89e71edd4ded8178bfbb5a464284e5ee89a02bcc
William Otieno <jimmywilliamotieno@gmail.com>
2020-05-26 15:21:05 +0300

    Added database

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/william/Dev/blog/blog.db'
+db = SQLAlchemy(app)
+
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(25))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
 
 
 @app.route('/')
